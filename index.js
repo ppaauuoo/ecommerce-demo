edaa6a3ae4df5922ea3b9a7ca4181b9a0edf0b91
ppaauuoo <bcg.channel@gmail.com>
2023-06-01 23:34:19 +0700

    currency system and lot

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const _ = require('lodash')
 const mongoose = require('mongoose');
+const alert = require('alert')
+const calculate = require(__dirname+'/calculate.js')
+
+
 
 const app = express();
 app.use(express.static("public"))
@@ -15,6 +19,7 @@ async function main() {
     await mongoose.connect('mongodb+srv://vorkna:OpOr2546@cluster0.vvkoeom.mongodb.net/shopDB');
   }
 
+  //goods
 const GoodsSchema = new mongoose.Schema({
     goodsName: String,
     goodsPrice: Number,
@@ -59,40 +64,50 @@ const CartSchema = new mongoose.Schema({
 
 const Cart = mongoose.model('Cart', CartSchema);
 
-const democart = new Cart({
-    goodsName: "test1",
-    goodsPrice: 2,
-    goodsDesc: "testdesc1",
-    goodsQuantity: 2,
-    goodsSubTotal: 2*2
+//money placeholder
+const WalletSchema = new mongoose.Schema({
+    money: Number,
+    point: Number,
+    total: Number
+})
+const Wallet = mongoose.model('Wallet', WalletSchema)
+const startWallet = new Wallet( {
+    money: 0,
+    point: 0,
+    total: 0
 })
 
 
 app.get('/',async (req,res)=>{
-
+    const wallet = await Wallet.find({})
     const product = await Goods.find({})
+
 
     if(product.length==0){
         await Goods.insertMany(demoProduct)
-        await democart.save()
     }
 
+    if(wallet.length==0){
+        await startWallet.save()
+    }
     res.render("home",{
-    goods: product
+        goods: product,
+        wallet: wallet
    })
 })
 
 app.post('/', async (req,res) => {
-    const requestGoods=req.body.Selecteditem
+    const requestGoods=req.body.selectedItem
+    
     const cart = await Cart.find({goodsName: requestGoods});
     const product = await Goods.find({goodsName: requestGoods});
 
-    console.log(cart)
-    if(cart[0]){
+
+    if(cart.length>0){
         const newQuantity=cart[0].goodsQuantity+1
-        const newSubTotal=cart[0].goodsPrice*cart[0].goodsQuantity;
+        const newSubTotal=cart[0].goodsPrice*newQuantity;
         await Cart.findOneAndUpdate({goodsName: requestGoods},{goodsQuantity: newQuantity, goodsSubTotal: newSubTotal},{returnOriginal:false})
-        console.log('got dupe')
+        
     }else{
         const newCart = new Cart({
             goodsName: product[0].goodsName,
@@ -102,29 +117,74 @@ app.post('/', async (req,res) => {
             goodsSubTotal: product[0].goodsPrice
         });
         await newCart.save()
-        console.log('got new stuff')
-    }
 
+    }
+    const currentCart = await Cart.find({});
+    await Wallet.findOneAndUpdate({},{total: calculate.totalCalculate(currentCart)},{returnOriginal:false})
     res.redirect('/')
     
 })
 
 
-
 app.get('/cart', async (req,res) => {
     const cart = await Cart.find({})
-    let total = 0;
-    cart.forEach((element) => {
-        total+=element.goodsSubTotal
-    })
+    await Wallet.findOneAndUpdate({},{total: calculate.totalCalculate(cart)},{returnOriginal:false})
+    const wallet = await Wallet.find({})
+    
     res.render("cart",{
         cart: cart,
-        total: total
+        wallet: wallet
     })
 })
 
-app.get('/cart',function(req,res){
+app.post('/cart',function(req,res){
     res.redirect("/cart")
+})
+
+app.get('/money', async (req,res) => {
+    const wallet = await Wallet.find({})
+    res.render("money",{
+        wallet: wallet
+    })
+})
+
+app.get('/money/add', async (req,res) => {
+    const wallet = await Wallet.find({})
+    await Wallet.findOneAndUpdate({},{money: wallet[0].money+100},{returnOriginal:false})
+    res.redirect('/money')
+})
+
+app.get('/money/transfer', async (req,res) => {
+    const wallet = await Wallet.find({})
+    if(wallet[0].point>=100){
+        await Wallet.findOneAndUpdate({},{money: wallet[0].money+20,point: wallet[0].point-100},{returnOriginal:false})
+        alert("Success!");
+        res.redirect('/money')
+    }else{
+        alert("Not Enough!");
+        res.redirect('/money')
+    }
+})
+
+
+app.get('/cart/checkout', async (req,res) => {
+    const wallet = await Wallet.find({})
+    if(wallet[0].money<wallet[0].total){
+        alert("Not Enough!");
+        res.redirect('/cart')
+    }else{
+        const pointObtained = calculate.pointCalculate(wallet[0].total)
+        await Wallet.findOneAndUpdate({},{money: wallet[0].money-wallet[0].total,point: wallet[0].point+pointObtained},{returnOriginal:false})
+        await Cart.deleteMany({ goodsSubTotal: { $gte: 0 } });
+        alert("you got "+pointObtained+" point!");
+        res.redirect('/')
+    }
+})
+
+app.post('/cart/delete', async (req,res) => {
+    const request2DeleteGoods=req.body.selected2DeleteItem
+    await Cart.deleteOne({ goodsName: request2DeleteGoods });
+    res.redirect('/cart')
 })
 
 
