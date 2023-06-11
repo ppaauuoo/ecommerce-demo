982e6d7abfc281109210f93718922c5c4a19417d
ppaauuoo <bcg.channel@gmail.com>
2023-06-12 05:49:05 +0700

    add ability to look for underling

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose = require("mongoose");
+const ObjectId = require('mongodb').ObjectId; 
+
 
 const session = require("express-session");
 const passport = require("passport");
@@ -57,6 +59,7 @@ const UserSchema = new mongoose.Schema({
   password: String,
   userWallet: WalletSchema,
   userCart: [CartSchema],
+  children: []
 });
 UserSchema.plugin(passportLocalMongoose);
 //initiialize user model
@@ -79,8 +82,7 @@ let isLogin = false;
 
 router.get("/", async (req, res) => {
   const product = await Goods.find({});
-  if (isLogin) {
-    
+  if (req.isAuthenticated()) {
     const updatedTotal = await calculate.totalCalculate(req.user.userCart);
     const updating = await User.findOneAndUpdate(
       { _id: req.user._id },
@@ -95,8 +97,6 @@ router.get("/", async (req, res) => {
       isLogin: isLogin,
     });
   } else {
-    
-
     res.render("home", {
       goods: product,
       isLogin: isLogin,
@@ -105,56 +105,54 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  if (isLogin == false) {
-    res.render("login");
-    return;
-  }
+  if (req.isAuthenticated()) {
 
-  const requestGoods = req.body.selectedItem;
+    const requestGoods = req.body.selectedItem;
 
-  const cart = req.user.userCart;
+    const cart = req.user.userCart;
 
-  const product = await Goods.find({ goodsName: requestGoods });
+    const product = await Goods.find({ goodsName: requestGoods });
 
-  const dupe = calculate.searchCart(cart, requestGoods);
+    const dupe = calculate.searchCart(cart, requestGoods);
 
 
-  if (dupe >= 0) {
-    console.log('dupe! '+dupe)
-    const newQuantity = cart[dupe].goodsQuantity + 1;
-    const newSubTotal = cart[dupe].goodsPrice * newQuantity;
-
-    await User.findOneAndUpdate(
-      { _id: req.user._id, "userCart.goodsName": requestGoods},
-      {
-        $set: {
-          "userCart.$.goodsQuantity": newQuantity,
-          "userCart.$.goodsSubTotal": newSubTotal
-        },
-      },
-      { returnOriginal: false }
-    );
-  } else {
-    console.log('no dupe! '+dupe)
-    await User.findOneAndUpdate(
-      { _id: req.user._id },
-      {
-        $push: {
-          userCart: {
-            goodsName: product[0].goodsName,
-            goodsPrice: product[0].goodsPrice,
-            goodsDesc: product[0].goodsDesc,
-            goodsQuantity: 1,
-            goodsSubTotal: product[0].goodsPrice,
+    if (dupe >= 0) {
+      const newQuantity = cart[dupe].goodsQuantity + 1;
+      const newSubTotal = cart[dupe].goodsPrice * newQuantity;
+      await User.findOneAndUpdate(
+        { _id: req.user._id, "userCart.goodsName": requestGoods},
+        {
+          $set: {
+            "userCart.$.goodsQuantity": newQuantity,
+            "userCart.$.goodsSubTotal": newSubTotal
           },
         },
-      },
-      { returnOriginal: false }
-    );
+        { returnOriginal: false }
+      );
+    } else {
+      await User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: {
+            userCart: {
+              goodsName: product[0].goodsName,
+              goodsPrice: product[0].goodsPrice,
+              goodsDesc: product[0].goodsDesc,
+              goodsQuantity: 1,
+              goodsSubTotal: product[0].goodsPrice,
+            },
+          },
+        },
+        { returnOriginal: false }
+      );
+    }
+    res.redirect("/");
+  }else{
+    res.redirect("/login");
   }
 
 
-  res.redirect("/");
+  
 });
 
 router.get("/login", (req, res) => {
@@ -171,21 +169,22 @@ router.post("/login", function (req, res) {
     if (err) { 
       alert('Try Again!')
       return next(err); }
-    return res.redirect('/secrets');
+    return res.redirect('/security');
   });
 });
 
+router.get("/security", (req,res)=>{
+  if(req.isAuthenticated()){
+    isLogin=true
+    res.redirect('/')
+  }else{
+    res.redirect('/login')
+  }
+
+})
+
 router.get("/register", (req, res) => {
   res.render("register");
-});
-
-router.get("/secrets", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect("/");
-    isLogin = true;
-  } else {
-    res.redirect("/login");
-  }
 });
 
 router.post("/register", async (req, res) => {
@@ -199,7 +198,7 @@ router.post("/register", async (req, res) => {
   User.register(newUser, password, function (err, user) {
     //A new user was saved
     passport.authenticate("local")(req, res, function () {
-      res.redirect("/secrets");
+      res.redirect("/mlm");
     });
   });
 });
@@ -213,10 +212,6 @@ router.get("/logout", (req, res, next) => {
 });
 
 router.get("/money", async (req, res) => {
-  if (isLogin == false) {
-    res.redirect("/");
-    return;
-  }
   if (req.isAuthenticated()) {
     res.render("money", {
       wallet: req.user.userWallet,
@@ -259,10 +254,6 @@ router.get("/money/transfer", async (req, res) => {
 });
 
 router.get("/cart", async (req, res) => {
-  if (isLogin == false) {
-    res.redirect("/");
-    return;
-  }
   if (req.isAuthenticated()) {
     const updatedTotal = await calculate.totalCalculate(req.user.userCart);
     const updating = await User.findOneAndUpdate(
@@ -323,6 +314,51 @@ router.post("/cart/delete", async (req, res) => {
   const request2DeleteGoods = req.body.selected2DeleteItem;
   await User.findOneAndUpdate({_id: req.user._id}, { $pull: { userCart: {goodsName: request2DeleteGoods }}},{ returnOriginal: false });
   res.redirect("/cart");
+});
+
+router.get("/mlm", async (req, res) => {
+  if (req.isAuthenticated()) {
+    await User.findOneAndUpdate({$expr: {$lt: [{ $size: "$children" }, 2]}},
+    { $push: { children: {
+      _id: req.user._id,
+      firstName: req.user.firstName
+    }}},
+    { returnOriginal: false })
+    isLogin = true;
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/user", async (req, res) => {
+  if (req.isAuthenticated()) {
+    res.redirect('/'+req.user._id)
+  }else{
+    res.redirect("/login");
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  const id = new ObjectId(req.params.userId)
+  const currentUser = await User.findById({_id: id})
+  var lowerUser1 = {children: []}
+  var lowerUser2 = {children: []}
+  if(currentUser.children[0]){
+    lowerUser1 = await User.findById({_id: currentUser.children[0]._id})
+  }
+  if(currentUser.children[1]){
+    lowerUser2 = await User.findById({_id: currentUser.children[1]._id})
+  }
+  
+
+  res.render('user', {
+    userName: currentUser.firstName,
+    lowerUser1: lowerUser1,
+    lowerUser2: lowerUser2,
+    isLogin: isLogin,
+    wallet: req.user.userWallet
+  })
 });
 
 module.exports = router;
