b7cf90dd73b564b2eef168767f1aabf963d16c68
ppaauuoo <bcg.channel@gmail.com>
2023-06-22 06:00:30 +0700

    migrating good to sql

--- database.js
+++ database.js
@@ -1,32 +1,18 @@
-  const express = require("express");
-  const router = express.Router();
-  const mongoose = require("mongoose");
-  // main().catch((err) => console.log(err));
-  // async function main() {
-  //   await mongoose.connect(
-  //     "mongodb+srv://vorkna:OpOr2546@cluster0.vvkoeom.mongodb.net/shopDB"
-  //   );
-  // }
+const mysql = require('mysql')
+const con = mysql.createConnection({
+  host: 'cpanel14wh.bkk1.cloud.z.com',
+  user: 'cp906134_vorkna',
+  password: 'OpOr2546',
+  database: 'cp906134_test'
+})
+
+// connection.connect()
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 
-  const Test = mongoose.model("Goods");
-
-  router.get("/test", async (req, res) => {
-    res.render("test",{
-      test: "dog"
-    })
-  });
-
-  router.post("/test", async (req, res) => {
-    if(action='fetch'){
-      const test = await Test.find({})
-      res.json({
-        test: test
-      })
-    }
-
-  });
-
-
-  module.exports = router;
+module.exports = con;
 
--- routes/home.js
+++ routes/home.js
@@ -2,79 +2,93 @@ const express = require("express");
 const router = express.Router();
 
 const mongoose = require("mongoose");
+const calculate = require("../helper/calculate.js");
 
-const calculate = require("../helper/calculate.js")
+var con = require("../database");
 
 const User = mongoose.model("User");
 const Goods = mongoose.model("Goods");
 
+
 router.get("/", async (req, res) => {
-    const product = await Goods.find({});
-    if (!req.isAuthenticated()) {
-      res.render("home", {
-        goods: product,
-        user: null
-      });
-      return
-    }
-    const updatedTotal = await calculate.totalCalculate(req.user.userCart);
-    const updating = await User.findOneAndUpdate(
-      { _id: req.user._id },
-      { $set: { "userWallet.total": updatedTotal } },
-      { returnOriginal: false, returnNewDocument: true }
-    );
-    const wallet = updating.userWallet;
-    res.render("home", {
-      goods: product,
-      wallet: wallet,
-      user: req.user
+  //const product = await Goods.find({});
+  const goods = await new Promise((resolve, reject) => {
+    con.query("SELECT * FROM goods ", (err, rows) => {
+      resolve(rows);
     });
   });
-  
-  router.post("/", async (req, res) => {
-    if (!req.isAuthenticated()) {
-      res.redirect("/login");
-      return
-    }
-    const requestGoods = req.body.selectedItem;
-  
-    const cart = req.user.userCart;
-  
-    const product = await Goods.find({ goodsName: requestGoods });
-  
-    const dupe = calculate.searchCart(cart, requestGoods);
-  
-    if (dupe >= 0) {
-      const newQuantity = cart[dupe].goodsQuantity + 1;
-      const newSubTotal = cart[dupe].goodsPrice * newQuantity;
-      await User.findOneAndUpdate(
-        { _id: req.user._id, "userCart.goodsName": requestGoods },
-        {
-          $set: {
-            "userCart.$.goodsQuantity": newQuantity,
-            "userCart.$.goodsSubTotal": newSubTotal,
-          },
-        },
-        { returnOriginal: false }
-      );
-    } else {
-      await User.findOneAndUpdate(
-        { _id: req.user._id },
-        {
-          $push: {
-            userCart: {
-              goodsName: product[0].goodsName,
-              goodsPrice: product[0].goodsPrice,
-              goodsDesc: product[0].goodsDesc,
-              goodsQuantity: 1,
-              goodsSubTotal: product[0].goodsPrice,
-            },
-          },
-        },
-        { returnOriginal: false }
-      );
-    }
-    res.redirect("/");
+  console.log(goods)
+  if (!req.isAuthenticated()) {
+    setTimeout(() => {
+      res.render("home", {
+        goods: goods||null,
+        user: null,
+      });},100);
+    return;
+  }
+  const updatedTotal = calculate.totalCalculate(req.user.userCart);
+  const updating = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: { "userWallet.total": updatedTotal } },
+    { returnOriginal: false, returnNewDocument: true }
+  );
+  // const updating = await new Promise((resolve, reject) => {
+  //   con.query(goodsQuery, (err, rows) => {
+  //     resolve(rows);
+  //   });
+  // });
+  const wallet = updating.userWallet;
+  res.render("home", {
+    goods: product,
+    wallet: wallet,
+    user: req.user,
   });
+});
 
-  module.exports = router;
+router.post("/", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    res.redirect("/login");
+    return;
+  }
+  const requestGoods = req.body.selectedItem;
+
+  const cart = req.user.userCart;
+
+  const product = await Goods.find({ goodsName: requestGoods });
+
+  const dupe = calculate.searchCart(cart, requestGoods);
+
+  if (dupe >= 0) {
+    const newQuantity = cart[dupe].goodsQuantity + 1;
+    const newSubTotal = cart[dupe].goodsPrice * newQuantity;
+    await User.findOneAndUpdate(
+      { _id: req.user._id, "userCart.goodsName": requestGoods },
+      {
+        $set: {
+          "userCart.$.goodsQuantity": newQuantity,
+          "userCart.$.goodsSubTotal": newSubTotal,
+        },
+      },
+      { returnOriginal: false }
+    );
+  } else {
+    await User.findOneAndUpdate(
+      { _id: req.user._id },
+      {
+        $push: {
+          userCart: {
+            goodsName: product[0].goodsName,
+            goodsPrice: product[0].goodsPrice,
+            goodsDesc: product[0].goodsDesc,
+            goodsQuantity: 1,
+            goodsSubTotal: product[0].goodsPrice,
+          },
+        },
+      },
+      { returnOriginal: false }
+    );
+  }
+  res.redirect("/");
+});
+
+module.exports = router;
